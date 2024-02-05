d4167f76a6310c4376775d151c67e87f465cba64
Khaos Uve <89417804+BibianPaniaguaVenturaJose@users.noreply.github.com>
2024-02-05 14:09:38 -0600

    Add files via upload
    Paso 9 hecho, pero me marco que fallo

---
+++ src/androidTest/java/com/example/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.example.fake
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NetworkMarsRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runBlocking {
+        val repository = NetworkMarsPhotosRepository(
+            marsApiService = FakeMarsApiService()
+        )
+        assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+
+    }
+
+}
